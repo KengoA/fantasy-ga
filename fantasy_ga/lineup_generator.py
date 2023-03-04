ff2620c0018d31894b4e6901ac3ab8623d9b240a
KengoA <20113339+KengoA@users.noreply.github.com>
2023-03-04 23:28:35 +0000

    use states for lineups and scores

--- fantasy_ga/lineup_generator.py
+++ fantasy_ga/lineup_generator.py
@@ -21,24 +21,25 @@ class LineupGenerator:
         self.n_compound = n_compound
         self.pos_start_idx = pos_start_idx
         self.sal_cap = sal_cap
+        self.lineups = []
+        self.scores = []
 
-    @staticmethod
-    def get_top_n_lineups(lineups, scores, n):
-        top_n_scores = (-scores).argsort()[:n]
-        return lineups.take(top_n_scores.astype(int), 0), scores.take(
+    def get_top_n_lineups(self, n):
+        top_n_scores = (-self.scores).argsort()[:n]
+        return self.lineups.take(top_n_scores.astype(int), 0), self.scores.take(
             top_n_scores.astype(int), 0
         )
 
-    def calc_scores(self, lineups: np.array):
+    def calc_scores(self):
         scores = []
-        for lineup in lineups:
+        for lineup in self.lineups:
             sal, fpts = self.m[np.in1d(self.m[:, 0], lineup.astype(int))][
                 :, [1, 2]
             ].sum(axis=0)
             scores.append(fpts) if sal <= self.sal_cap and len(
                 np.unique(lineup)
             ) == 8 else scores.append(-1)
-        return np.array(scores)
+        self.scores = np.array(scores)
 
     def create_random_lineups(self):
         lineups = []
@@ -56,48 +57,45 @@ class LineupGenerator:
             lineups.append(lineup)
         return np.array(lineups)
 
-    def breed(self, lineups, scores):
+    def breed(self):
         new_lineups = []
-        parents_idx = (-scores).argsort()[:2]
-        parents = lineups.take(parents_idx, 0)
+        parents_idx = (-self.scores).argsort()[:2]
+        parents = self.lineups.take(parents_idx, 0)
 
         for _ in range(self.n_breed):
             if np.array_equal(*parents):
-                return parents
+                self.lineups = parents
+                return
             else:
                 breed_idx = np.random.choice(2, 8)
                 new_lineups.append([parents[p, idx] for idx, p in enumerate(breed_idx)])
-        return np.vstack([parents, np.array(new_lineups)])
+        self.lineups = np.vstack([parents, np.array(new_lineups)])
 
-    def mutate(self, lineups):
-        mutate_idx = np.random.choice(lineups.shape[0], self.n_mutate)
+    def mutate(self):
+        mutate_idx = np.random.choice(self.lineups.shape[0], self.n_mutate)
 
         for idx in mutate_idx:
             mutant = self.m[np.random.choice(self.m.shape[0]), :]
             original = self.m[
-                self.m[:, 0] == np.random.choice(lineups[idx]).astype(int), :
+                self.m[:, 0] == np.random.choice(self.lineups[idx]).astype(int), :
             ][0]
             eligible_pos = np.where(
                 mutant[self.pos_start_idx :].astype(bool)
                 & original[self.pos_start_idx :].astype(bool)
             )[0]
             swap_pos = np.random.choice(eligible_pos)
-            lineups = np.vstack([lineups, lineups[idx]])
-            lineups[-1, swap_pos] = mutant[0]
-        return lineups
+            self.lineups = np.vstack([self.lineups, self.lineups[idx]])
+            self.lineups[-1, swap_pos] = mutant[0]
 
-    def evolve(self, lineups, scores):
+    def evolve(self):
         for _ in range(self.n_gen):
-            lineups = self.breed(lineups, scores)
-            lineups = self.mutate(lineups)
-            scores = self.calc_scores(lineups)
-        return lineups, scores
+            self.breed()
+            self.mutate()
+            self.calc_scores()
 
     def fit(self):
-        lineups = self.create_random_lineups()
-        scores = self.calc_scores(lineups)
+        self.lineups = self.create_random_lineups()
         for _ in range(self.n_compound):
-            lineups, scores = self.evolve(lineups, scores)
-            lineups = np.vstack([lineups, self.create_random_lineups()])
-            scores = self.calc_scores(lineups)
-        return lineups, scores
+            self.calc_scores()
+            self.evolve()
+            self.lineups = np.vstack([self.lineups, self.create_random_lineups()])
